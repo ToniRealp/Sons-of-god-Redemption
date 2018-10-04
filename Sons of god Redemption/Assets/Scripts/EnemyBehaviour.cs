491d5d726a0e73e3bb97dc870bfdd6e03ce84d34
Ares Fernández Ramos <ares.fernandez@enti.cat>
2018-10-04 19:37:09 +0200

    Enemy searching v1

--- Sons of god Redemption/Assets/Scripts/EnemyBehaviour.cs
+++ Sons of god Redemption/Assets/Scripts/EnemyBehaviour.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour {
 
+    public float walkingSpeed = 3;
     public float movingRange = 5;
-    public float changePosTime = 5;
+    public float changePosTime = 15;
+    public float distanceAproach = 0.1f; // Never put this to 0
+    public float rotationSpeed = 0.05f;
+
 
     private Vector3 initialPosition;
-    private Vector3 destinationPosition;
-    private float xMin, xMax, zMin, zMax;
+    public Vector3 destinationPosition;
+    public Vector3 travelVector;
+    private float xMin, xMax, zMin, zMax, xDest, zDest;
     private float initTime;
+
+    enum State { SEARCHING, SEEKING, ATTAKING };
+    State state = State.SEARCHING;
 
     void setMovementRange(float _x, float _z){
         xMin = _x - movingRange;
@@ -23,15 +31,49 @@ public class EnemyBehaviour : MonoBehaviour {
 	void Start () {
         initialPosition = this.GetComponent<Transform>().position;
         setMovementRange(initialPosition.x, initialPosition.z);
+        destinationPosition.x = xDest = Random.Range(xMin, xMax);
+        destinationPosition.y = initialPosition.y;
+        destinationPosition.z = zDest = Random.Range(zMin, zMax);
         initTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Time.time - initTime >= changePosTime)
-        {
 
+        switch (state)
+        {
+            case State.SEARCHING:
+
+                // Calculate travel vector
+                travelVector = new Vector3(destinationPosition.x - this.GetComponent<Transform>().position.x,0, destinationPosition.z - this.GetComponent<Transform>().position.z);
+
+                // Rotate and go to destiny location
+                if (travelVector.magnitude > distanceAproach) {
+                    this.GetComponent<Rigidbody>().rotation = Quaternion.Slerp(this.GetComponent<Rigidbody>().rotation, Quaternion.LookRotation(travelVector), rotationSpeed);
+                    this.GetComponent<Rigidbody>().velocity = new Vector3(travelVector.normalized.x*walkingSpeed, 0, travelVector.normalized.z*walkingSpeed);
+                }
+                else {
+                    this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                }
+
+
+                // Reset destiny location
+                if (Time.time - initTime >= changePosTime) {
+                    destinationPosition.x = xDest = Random.Range(xMin, xMax);
+                    destinationPosition.z = zDest = Random.Range(zMin, zMax);
+                    
+                    initTime = Time.time;
+                }
+
+                break;
+            case State.SEEKING:
+                break;
+            case State.ATTAKING:
+                break;
+            default:
+                break;
         }
+
 
 
 
