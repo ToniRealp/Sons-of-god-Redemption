bb98aa2054fe96dd784587b75b87fac4b9c251aa
Ares Fernández Ramos <ares.fernandez@enti.cat>
2018-10-10 18:35:50 +0200

    Enemy Searching Pathfinding implemented

--- Sons of god Redemption/Assets/Scripts/EnemyBehaviour.cs
+++ Sons of god Redemption/Assets/Scripts/EnemyBehaviour.cs
@@ -1,26 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour {
 
-    public float walkingSpeed = 3;
     public float movingRange = 5;
-    public float changePosTime = 15;
-    public float distanceAproach = 0.1f; // Never put this to 0
-    public float rotationSpeed = 0.05f;
-
+    public float changePosTime = 5;
+    public NavMeshAgent NavAgent;
 
     private Vector3 initialPosition;
-    public Vector3 destinationPosition;
-    public Vector3 travelVector;
-    private float xMin, xMax, zMin, zMax, xDest, zDest;
+    private Vector3 destinationPosition;
+    private float xMin, xMax, zMin, zMax;
     private float initTime;
 
     enum State { SEARCHING, SEEKING, ATTAKING };
     State state = State.SEARCHING;
 
-    void setMovementRange(float _x, float _z){
+    void SetMovementRange(float _x, float _z){
+
         xMin = _x - movingRange;
         xMax = _x + movingRange;
         zMin = _z - movingRange;
@@ -30,10 +28,8 @@ public class EnemyBehaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         initialPosition = this.GetComponent<Transform>().position;
-        setMovementRange(initialPosition.x, initialPosition.z);
-        destinationPosition.x = xDest = Random.Range(xMin, xMax);
-        destinationPosition.y = initialPosition.y;
-        destinationPosition.z = zDest = Random.Range(zMin, zMax);
+        SetMovementRange(initialPosition.x, initialPosition.z);
+        destinationPosition = new Vector3(Random.Range(xMin, xMax), 5 , Random.Range(zMin, zMax));
         initTime = Time.time;
 	}
 	
@@ -44,29 +40,26 @@ public class EnemyBehaviour : MonoBehaviour {
         {
             case State.SEARCHING:
 
-                // Calculate travel vector
-                travelVector = new Vector3(destinationPosition.x - this.GetComponent<Transform>().position.x,0, destinationPosition.z - this.GetComponent<Transform>().position.z);
-
-                // Rotate and go to destiny location
-                if (travelVector.magnitude > distanceAproach) {
-                    this.GetComponent<Rigidbody>().rotation = Quaternion.Slerp(this.GetComponent<Rigidbody>().rotation, Quaternion.LookRotation(travelVector), rotationSpeed);
-                    this.GetComponent<Rigidbody>().velocity = new Vector3(travelVector.normalized.x*walkingSpeed, 0, travelVector.normalized.z*walkingSpeed);
-                }
-                else {
-                    this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
-                }
-
-
                 // Reset destiny location
                 if (Time.time - initTime >= changePosTime) {
-                    destinationPosition.x = xDest = Random.Range(xMin, xMax);
-                    destinationPosition.z = zDest = Random.Range(zMin, zMax);
+                    destinationPosition.x = Random.Range(xMin, xMax);
+                    destinationPosition.z = Random.Range(zMin, zMax);
                     
                     initTime = Time.time;
                 }
 
+                // Go to destination
+                NavAgent.SetDestination(destinationPosition);
+
                 break;
             case State.SEEKING:
+
+
+
+                // Go to destination
+                NavAgent.SetDestination(destinationPosition);
+
+
                 break;
             case State.ATTAKING:
                 break;
