53944652c426f147e7a3157e640246d4af5804d0
Antoni Realp Serrano <antoni.realp@enti.cat>
2018-10-28 19:43:05 +0100

    Upgraded cameras to cinemachine

---
+++ Sons of god Redemption/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+   public GameObject cameraTrigger,cam1,cam2;
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true); 
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+        }
+    }
+}
