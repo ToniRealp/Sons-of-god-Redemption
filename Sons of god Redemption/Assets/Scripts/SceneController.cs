c7aa782cfcbfddee7ac6f3047efd4b1f56382d23
Ares Fernández Ramos <ares.fernandez@enti.cat>
2018-11-03 13:15:19 +0100

    Menu Placeholder Implemented

---
+++ Sons of god Redemption/Assets/Scripts/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void exitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
