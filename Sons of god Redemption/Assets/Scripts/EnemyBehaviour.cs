f22601aa3bd38c31aef5cf19f5ce8d37ce99273f
Ares Fernández Ramos <ares.fernandez@enti.cat>
2018-11-27 12:30:23 +0100

    Enemy Base Class Creation and first functions added
    Added functions:
    -SetSearchingRange
    -SetRandomDestination
    -MoveTo
    -ChangeSpeed
    -LookTo
    -IsMoving
    -DistanceTo
    -UpdateRaycasts
    -DebugRaycasts
    -DetectPlayer
    -UseFullDetectionSystem
    -UpdateHealthText
    -Die
    -AnimationLength

--- Sons of god Redemption/Assets/Scripts/EnemyBehaviour.cs
+++ Sons of god Redemption/Assets/Scripts/EnemyBehaviour.cs
@@ -91,8 +91,8 @@ public class EnemyBehaviour : MonoBehaviour {
 
         if (health <= 0)
         {
-            Destroy(this.gameObject);
             Destroy(healthTextGO);
+            Destroy(this.gameObject);
         }
 
         // Health text update
