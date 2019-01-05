80b9d32a97cb2e7a00d5d9a6f9c67a44fb0ca663
Ares Fernández Ramos <ares.fernandez@enti.cat>
2019-01-05 16:59:57 +0100

    Press Start Scene and Splash Screen Logo addded

---
+++ Sons of god Redemption/Assets/PressStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressStart : MonoBehaviour {
+
+    public InputManager inputManager;
+    public SceneController sceneController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inputManager.attackButton || inputManager.dashButton || inputManager.escape || inputManager.interact || inputManager.strongAttackButton)
+        {
+            sceneController.changeScene("MainMenu");
+        }
+	}
+}
