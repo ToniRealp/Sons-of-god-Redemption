f513324411729fd98f8be283716aad54840d7878
Ares Fernández Ramos <ares.fernandez@enti.cat>
2019-01-05 14:03:14 +0100

    Save Commit pause Menu

---
+++ Sons of god Redemption/Assets/PauseScriptManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScriptManager : MonoBehaviour {
+
+    public GameObject PausePanel;
+
+	// Use this for initialization
+	void Start () {
+        PausePanel.SetActive(false);
+	}
+
+    void activatePauseMenu()
+    {
+        Time.timeScale = 0f;
+        PausePanel.SetActive(true);
+    }
+
+    void exitPauseMenu()
+    {
+        Time.timeScale = 1.0f;
+        PausePanel.SetActive(false);
+    }
+
+}
