bde3f9754e07d407fddf1b4994b5c70a505c17f6
Ares Fernández Ramos <ares.fernandez@enti.cat>
2019-01-05 17:38:22 +0100

    Press Start modified and win scene added

--- Sons of god Redemption/Assets/Scripts/PressStart.cs
+++ Sons of god Redemption/Assets/Scripts/PressStart.cs
@@ -7,16 +7,21 @@ public class PressStart : MonoBehaviour {
     public InputManager inputManager;
     public SceneController sceneController;
 
+    private float startTime;
+
 	// Use this for initialization
 	void Start () {
-		
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (inputManager.attackButton || inputManager.dashButton || inputManager.escape || inputManager.interact || inputManager.strongAttackButton)
+        if (Time.time>= startTime+1.0f)
         {
-            sceneController.changeScene("MainMenu");
+            if (inputManager.attackButton || inputManager.dashButton || inputManager.escape || inputManager.interact || inputManager.strongAttackButton)
+            {
+                sceneController.changeScene("MainMenu");
+            }
         }
 	}
 }
