0fa24e35c2d0e681132f639b5297fa595d9faf6e
Antoni Realp Serrano <antoni.realp@enti.cat>
2019-01-05 18:24:39 +0100

    Implemented loading screen

---
+++ Sons of god Redemption/Assets/Scripts/GameManagement/LoadingScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public SceneController sceneController;
+
+	// Update is called once per frame
+	void Update () {
+
+        loadingScreen.SetActive(sceneController.isLoading);
+        slider.value = sceneController.progress;
+
+    }
+}
--- Sons of god Redemption/Assets/Scripts/GameManagement/SceneController.cs
+++ Sons of god Redemption/Assets/Scripts/GameManagement/SceneController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class SceneController : MonoBehaviour {
+
+    public bool isLoading;
+    public float progress;
 
     public void changeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadAsynchronously(sceneName));
+        isLoading = true;
+    }
+
+    IEnumerator LoadAsynchronously (string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        while (!operation.isDone)
+        {
+            progress = Mathf.Clamp01(operation.progress / .9f);
+            yield return null;
+        }
     }
 
     public void exitGame()
