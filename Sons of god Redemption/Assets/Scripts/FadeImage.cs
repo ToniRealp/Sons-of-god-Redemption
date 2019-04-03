b93b10d8069113debfb726e9bfeea3ccca5a09fa
Ares Fernández Ramos <ares.fernandez@enti.cat>
2019-04-03 18:36:35 +0200

    Added Background to Tutorial texts

---
+++ Sons of god Redemption/Assets/Scripts/FadeImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeImage : MonoBehaviour {
+
+    public float dieTime = 5;
+    public Image image;
+
+    private Color c;
+
+    // Use this for initialization
+    void Start()
+    {
+        c = image.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dieTime -= Time.deltaTime;
+        if (dieTime <= 0)
+        {
+            c.a -= 0.01f;
+
+            image.color = c;
+        }
+
+    }
+}
