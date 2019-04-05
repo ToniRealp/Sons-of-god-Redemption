6015d3172a6dbbc7caf07451da2f720272b47d6a
Antoni Realp Serrano <antoni.realp@enti.cat>
2019-04-05 19:31:03 +0200

    First boss cinematic presentation implemented

---
+++ Sons of god Redemption/Assets/Scripts/Camera/BossCinematic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCinematic : MonoBehaviour {
+
+    public GameObject cam1, cam2;
+
+    void ChangeCamera()
+    {
+        cam1.SetActive(false);
+        cam2.SetActive(true);
+    }
+    void ResetCamera()
+    {
+        cam1.SetActive(true);
+        cam2.SetActive(false);
+    }
+}
