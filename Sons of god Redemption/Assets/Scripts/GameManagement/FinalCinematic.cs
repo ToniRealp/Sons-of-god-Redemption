4f834f32d4e5918e3a079eb8b19498b65f8fc1d5
AresFernandez <ares.fernandez@enti.cat>
2019-04-07 14:01:54 +0200

    FinalNarrative added

---
+++ Sons of god Redemption/Assets/Scripts/GameManagement/FinalCinematic.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalCinematic : MonoBehaviour {
+
+
+    public InputManager inputManager;
+    public SceneController sceneController;
+    public Image image;
+
+    public float startTime, textInScreenTime = 4.0f;
+    private int imageNum;
+    private bool textShown;
+    private Color c;
+
+    // Use this for initialization
+    void Start()
+    {
+        textShown = false;
+        c = image.color;
+        c.a = 0;
+        image.color = c;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        c = image.color;
+        if (image.color.a < 1 && !textShown)
+        {
+            c.a += 0.01f;
+            if (inputManager.attackButton || inputManager.dashButton || inputManager.interact || inputManager.strongAttackButton) { c.a = 1; }
+            image.color = c;
+            startTime = Time.time;
+        }
+        else
+        {
+            textShown = true;
+        }
+        if (textShown && (Time.time - startTime >= textInScreenTime || inputManager.attackButton || inputManager.dashButton || inputManager.interact || inputManager.strongAttackButton))
+        {
+            c.a -= 0.01f;
+            if (inputManager.attackButton || inputManager.dashButton || inputManager.interact || inputManager.strongAttackButton) { c.a = 0; }
+            image.color = c;
+            if (image.color.a <= 0)
+            {
+                sceneController.changeScene("MainMenu");
+            }
+        }
+
+    }
+}
