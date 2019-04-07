d3923eaf955ac58d5e0170ed4cb40fb1a204565c
Antoni Realp Serrano <antoni.realp@enti.cat>
2019-04-07 19:47:37 +0200

    Second boss cinematic

--- Sons of god Redemption/Assets/Scripts/Camera/BossCinematic.cs
+++ Sons of god Redemption/Assets/Scripts/Camera/BossCinematic.cs
@@ -6,6 +6,13 @@ public class BossCinematic : MonoBehaviour {
 
     public GameObject cam1, cam2;
 
+    private void Start()
+    {
+        cam1 = GameObject.Find("CM vcam1");
+        cam2 = GameObject.Find("Cinematic");
+        cam2.SetActive(false);
+    }
+
     public void ChangeCamera()
     {
         cam1.SetActive(false);
