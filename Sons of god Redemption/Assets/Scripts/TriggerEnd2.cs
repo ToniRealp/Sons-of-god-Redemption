ed69aa181ca78dc9e0326e102535ce9fdebb0aba
AresFernandez <ares.fernandez@enti.cat>
2019-04-07 19:10:33 +0200

    EndDoor trigger to level 3 added

---
+++ Sons of god Redemption/Assets/Scripts/TriggerEnd2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnd2 : MonoBehaviour {
+
+    public SceneController sceneController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            sceneController.changeScene("LevelGame3");
+        }
+    }
+}
