05e6da0d0ac04f7445650c21f12aaee20914c976
Ares Fernández Ramos <ares.fernandez@enti.cat>
2019-05-02 17:31:53 +0200

    Tutorial improvement finished

--- Sons of god Redemption/Assets/Scripts/GameManagement/TutorialController.cs
+++ Sons of god Redemption/Assets/Scripts/GameManagement/TutorialController.cs
@@ -6,15 +6,15 @@ public class TutorialController : MonoBehaviour {
 
     public float rememberTextTime = 10, waitTime = 1;
 
-    public GameObject wasd, lightAt, strongAt, lightElement, lightUtility, fireElement, fireUtility,
-        darkElement, darkUtility, dash, goodJob;
+    public GameObject player, wasd, lightAt, strongAt, lightElement, lightUtility, fireElement, fireUtility,
+        darkElement, darkUtility, heal, dash, goodJob;
     public GameObject enemy, uselessEnemy;
     private InputManager inputManager;
     public int enemyCounter;
 
-    public bool LightAt, Dash, StrongAt, Element, openDoor, wait;
-    private int state;
-    private float startTime, startWait;
+    public bool LightAt, Dash, StrongAt, wait, condition;
+    public int state;
+    public float startTime, startWait;
 
 	// Use this for initialization
 	void Start () {
@@ -23,7 +23,7 @@ public class TutorialController : MonoBehaviour {
         inputManager = GetComponent<InputManager>();
 
         //Initialization for the enemies spawns
-        wait = LightAt = openDoor = Dash = StrongAt = Element = false;
+        condition = wait = LightAt = Dash = StrongAt = false;
         enemyCounter = 0;
 
         //Initialization for the tutorial texts
@@ -55,7 +55,6 @@ public class TutorialController : MonoBehaviour {
         {
             enemyCounter++;
         }
-        Debug.Log("State: " + state);
         if (state == 0)
         {
             if (Time.time - startTime > rememberTextTime)
@@ -85,13 +84,13 @@ public class TutorialController : MonoBehaviour {
                 startTime = Time.time;
                 lightAt.GetComponent<TutorialTextScript>().ScaleText();
             }
-            if (inputManager.attackButton)
+            if (inputManager.attackButton && !condition)
             {
                 LightAt = true;
+                condition = true;
             }
             if (LightAt && enemyCounter == 0)
             {
-
                 lightAt.SetActive(false);
                 strongAt.SetActive(true);
                 startWait = Time.time;
@@ -103,6 +102,7 @@ public class TutorialController : MonoBehaviour {
                 DoSpawnUselessEnemy();
                 startTime = Time.time;
                 state++;
+                condition = false;
                 wait = false;
             }
         }
@@ -113,16 +113,60 @@ public class TutorialController : MonoBehaviour {
                 startTime = Time.time;
                 strongAt.GetComponent<TutorialTextScript>().ScaleText();
             }
-            if (inputManager.strongAttackButton)
+            if (inputManager.strongAttackButton && !condition)
             {
                 StrongAt = true;
+                condition = true;
             }
-            if (strongAt && enemyCounter == 0)
+            if (StrongAt && enemyCounter == 0)
             {
                 strongAt.SetActive(false);
-                lightElement.SetActive(true);
+                dash.SetActive(true);
                 startWait = Time.time;
                 StrongAt = false;
+                wait = true;
+            }
+            if (wait && Time.time - startWait > waitTime)
+            {
+                startTime = Time.time;
+                state++;
+                condition = false;
+                wait = false;
+            }
+        }
+        else if (state == 3)
+        {
+            if (Time.time - startTime > rememberTextTime)
+            {
+                startTime = Time.time;
+                dash.GetComponent<TutorialTextScript>().ScaleText();
+            }
+            if (inputManager.dashButton)
+            {
+                dash.SetActive(false);
+                lightElement.SetActive(true);
+                startWait = Time.time;
+                wait = true;
+            }
+            if (wait && Time.time - startWait > waitTime)
+            {
+                startTime = Time.time;
+                state++;
+                wait = false;
+            }
+        }
+        else if (state == 4)
+        {
+            if (Time.time - startTime > rememberTextTime)
+            {
+                startTime = Time.time;
+                lightElement.GetComponent<TutorialTextScript>().ScaleText();
+            }
+            if (inputManager.padXAxis == -1)
+            {
+                lightElement.SetActive(false);
+                lightUtility.SetActive(true);
+                startWait = Time.time;
                 wait = true;
             }
             if (wait && Time.time - startWait > waitTime)
@@ -133,68 +177,139 @@ public class TutorialController : MonoBehaviour {
                 wait = false;
             }
         }
+        else if (state == 5)
+        {
+            if (Time.time - startTime > rememberTextTime)
+            {
+                startTime = Time.time;
+                lightUtility.GetComponent<TutorialTextScript>().ScaleText();
+            }
+            if (player.GetComponent<PlayerController>().lightUI.activeSelf && enemyCounter == 0 && !condition)
+            {
+                lightUtility.SetActive(false);
+                fireElement.SetActive(true);
+                startWait = Time.time;
+                condition = true;
+                wait = true;
+            }
+            if (wait && Time.time - startWait > waitTime)
+            {
+                startTime = Time.time;
+                state++;
+                condition = false;
+                wait = false;
+            }
+        }
+        else if (state == 6)
+        {
+            if (Time.time - startTime > rememberTextTime)
+            {
+                startTime = Time.time;
+                fireElement.GetComponent<TutorialTextScript>().ScaleText();
+            }
+            if (inputManager.padXAxis == 1)
+            {
+                fireElement.SetActive(false);
+                fireUtility.SetActive(true);
+                startWait = Time.time;
+                wait = true;
+            }
+            if (wait && Time.time - startWait > waitTime)
+            {
+                DoSpawnUselessEnemy();
+                startTime = Time.time;
+                state++;
+                wait = false;
+            }
+        }
+        else if (state == 7)
+        {
+            if (Time.time - startTime > rememberTextTime)
+            {
+                startTime = Time.time;
+                fireUtility.GetComponent<TutorialTextScript>().ScaleText();
+            }
+            if (player.GetComponent<PlayerController>().fireUI.activeSelf && enemyCounter == 0 && !condition)
+            {
+                fireUtility.SetActive(false);
+                darkElement.SetActive(true);
+                startWait = Time.time;
+                condition = true;
+                wait = true;
+            }
+            if (wait && Time.time - startWait > waitTime)
+            {
+                startTime = Time.time;
+                state++;
+                condition = false;
+                wait = false;
+            }
+        }
+        else if (state == 8)
+        {
+            if (Time.time - startTime > rememberTextTime)
+            {
+                startTime = Time.time;
+                darkElement.GetComponent<TutorialTextScript>().ScaleText();
+            }
+            if (inputManager.padYAxis == -1)
+            {
+                darkElement.SetActive(false);
+                darkUtility.SetActive(true);
+                startWait = Time.time;
+                wait = true;
+            }
+            if (wait && Time.time - startWait > waitTime)
+            {
+                DoSpawnUselessEnemy();
+                startTime = Time.time;
+                state++;
+                wait = false;
+            }
+        }
+        else if (state == 9)
+        {
+            if (Time.time - startTime > rememberTextTime)
+            {
+                startTime = Time.time;
+                darkUtility.GetComponent<TutorialTextScript>().ScaleText();
+            }
+            if (player.GetComponent<PlayerController>().darkUI.activeSelf && enemyCounter == 0 && !condition)
+            {
+                darkUtility.SetActive(false);
+                heal.SetActive(true);
+                startWait = Time.time;
+                condition = true;
+                wait = true;
+            }
+            if (wait && Time.time - startWait > waitTime)
+            {
+                startTime = Time.time;
+                state++;
+                condition = false;
+                wait = false;
+            }
+        }
+        else if (state == 10)
+        {
+            if (Time.time - startTime > rememberTextTime)
+            {
+                startTime = Time.time;
+                heal.GetComponent<TutorialTextScript>().ScaleText();
+            }
+            if (inputManager.padYAxis == 1)
+            {
+                heal.SetActive(false);
+                goodJob.SetActive(true);
+                this.GetComponent<BoxCollider>().isTrigger = true;
+                state++;
+            }
+        }
+
 
 
         enemyCounter = 0;
 
-        //Spawn Light At Enemy
-        //       if ((firstEnemySpawnTime-=Time.deltaTime)<=0 && spawnLightAtEnemy)
-        //       {
-        //           Debug.Log("LightEnemy");
-        //           DoSpawnEnemy();
-        //           spawnLightAtEnemy = false;
-        //           spawnDashEnemy = true;
-        //           wasd.SetActive(false);
-        //           lightAt.SetActive(true);
-        //       }
-
-        //       //Spawn Dash Enemy
-        //       else if (enemyCounter == 0 && spawnDashEnemy)
-        //       {
-        //           Debug.Log("DashEnemy");
-        //           DoSpawnEnemy();
-        //           spawnDashEnemy = false;
-        //           spawnStrongAtEnemies = true;
-        //           lightAt.SetActive(false);
-        //           dash.SetActive(true);
-        //       }
-
-        //       //Spawn Strong At Enemies
-        //       else if (enemyCounter == 0 && spawnStrongAtEnemies)
-        //       {
-        //           Debug.Log("StrongEnemy");
-        //           DoSpawnEnemy();
-        //           DoSpawnEnemy();
-        //           spawnStrongAtEnemies = false;
-        //           spawnElementEnemies = true;
-        //           dash.SetActive(false);
-        //           strongAt.SetActive(true);
-        //       }
-
-        //       //Spawn Element Enemies
-        //       else if (enemyCounter == 0 && spawnElementEnemies)
-        //       {
-        //           Debug.Log("ElementEnemy");
-        //           DoSpawnEnemy();
-        //           DoSpawnEnemy();
-        //           spawnElementEnemies = false;
-        //           openDoor = true;
-        //           strongAt.SetActive(false);
-        //           elements.SetActive(true);
-        //       }
-
-        //       //Open Door
-        //       else if (enemyCounter == 0 && openDoor)
-        //       {
-        //           Debug.Log("OpenDoor");
-        //           gameObject.GetComponent<BoxCollider>().isTrigger = true;
-        //           openDoor = false;
-        //           elements.SetActive(false);
-        //           goodJob.SetActive(true);
-        //       }
-
-
-        //       enemyCounter = 0;
     }
 
 
