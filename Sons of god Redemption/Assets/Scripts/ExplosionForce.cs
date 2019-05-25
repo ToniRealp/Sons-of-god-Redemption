71921c28766b3ef31fa51fbc82456d3d8aa08f7a
Antoni Realp Serrano <antoni.realp@enti.cat>
2019-05-25 20:24:43 +0200

    Fixed explosion force for barriletes

--- Sons of god Redemption/Assets/Scripts/ExplosionForce.cs
+++ Sons of god Redemption/Assets/Scripts/ExplosionForce.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ExplosionForce : MonoBehaviour {
 
     public float minForce, maxForce, radius;
+    Vector3 tf;
+    
     // Use this for initialization
     private void Start()
     {
@@ -15,9 +17,14 @@ public class ExplosionForce : MonoBehaviour {
     {
         foreach(Transform t in transform)
         {
+            tf = transform.position;
             Rigidbody rb = t.GetComponent<Rigidbody>();
+            if (t.gameObject.tag == "Barrilete")
+            {
+                tf.z += 1.2f;
+            }
 
-            if (rb != null) rb.AddExplosionForce(Random.Range(minForce,maxForce), transform.position, radius);
+            if (rb != null) rb.AddExplosionForce(Random.Range(minForce,maxForce), tf, radius);
         }
         
     }
