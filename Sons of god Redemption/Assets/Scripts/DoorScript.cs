7e6d856008de534622ffcf3d3426633169a56b7c
Antoni Realp Serrano <antoni.realp@enti.cat>
2019-05-27 22:50:19 +0200

    door sound effect implemented

--- Sons of god Redemption/Assets/Scripts/DoorScript.cs
+++ Sons of god Redemption/Assets/Scripts/DoorScript.cs
@@ -6,6 +6,7 @@ public class DoorScript : MonoBehaviour {
 
     public Vector3 upPosition;
     public Vector3 downPosition;
+    private AudioManager audioManager;
     public float speed = 3f;
 
     public bool up, down;
@@ -17,12 +18,16 @@ public class DoorScript : MonoBehaviour {
         downPosition =  upPosition - new Vector3(0, 5.1f, 0);
         transform.position = downPosition;
         up = down = false;
+        audioManager = GetComponent<AudioManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (up)
         {
+            if (!audioManager.isPlaying("doorRumble"))
+                audioManager.Play("doorRumble");
+
             if (Vector3.Distance(transform.position, upPosition) >= speed*Time.deltaTime)
             {
                 transform.position = Vector3.MoveTowards(transform.position, upPosition, speed * Time.deltaTime);
@@ -34,6 +39,9 @@ public class DoorScript : MonoBehaviour {
         }
         if (down)
         {
+            if (!audioManager.isPlaying("doorRumble"))
+                audioManager.Play("doorRumble");
+
             if (Vector3.Distance(transform.position, downPosition) >= speed*Time.deltaTime)
             {
                 transform.position = Vector3.MoveTowards(transform.position, downPosition, speed * Time.deltaTime);
